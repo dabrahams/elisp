3a9e428ce99824b900eaa8ddf1fbe2b273b30436
David Abrahams <dave@boostpro.com>
2009-01-05 20:21:32 -0500

    No longer need rst-settings.el in emacs-23; rst is auto-mode'd and auto-load'd

--- elisp/autoload.d/rst-settings.el
+++
@@ -1,4 +0,0 @@
-   (autoload 'rst-mode "rst" "mode for editing reStructuredText documents" t)
-   (setq auto-mode-alist
-         (append '(("\\.rst$" . rst-mode)
-                   ("\\.rest$" . rst-mode)) auto-mode-alist))
