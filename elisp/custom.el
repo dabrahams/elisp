974f3d9217d1d820dfe06de23ef62c7afa3ba0af
David Abrahams <dave@boostpro.com>
2009-01-05 21:58:01 -0500

    Ediff settings moved entirely to custom.el

--- elisp/custom.el
+++ elisp/custom.el
@@ -48,8 +48,20 @@ typing, the region is replaced")
  '(diff-jump-to-old-file t)
  '(dired-listing-switches "-alh" nil nil "
 Added -h so I can read file sizes")
+ '(ediff-custom-diff-options "-u" nil nil "\"
+Show me unified diffs by default\"")
+ '(ediff-highlight-all-diffs nil nil nil "\"
+only highlight the selected diff (keeps down gray cruft onscreen)\"")
+ '(ediff-keep-variants t nil nil "\"
+Any unchanged buffers in the ediff are removed when the session ends. 
+`C-u q' to override when quitting.")
  '(ediff-merge-filename-prefix "")
  '(ediff-skip-merge-regions-that-differ-from-default nil)
+ '(ediff-split-window-function (quote split-window-horizontally) nil nil "\"
+Show diffs side-by-side\"")
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain) nil nil "\"
+Run Ediff all in one frame.  The default when there's a window manager is for
+emacs to pop up a separate frame for the `*Ediff Control Panel*' buffer\"")
  '(erc-modules (quote (autoaway autojoin button completion fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring smiley sound stamp track)))
  '(gdb-max-frames 100 nil nil "
 Increased the number of stack frames displayed from 40")
