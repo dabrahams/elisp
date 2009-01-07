795487803c507ef7fd46dee7f9346e4b4026280c
David Abrahams <dave@boostpro.com>
2009-01-07 06:45:22 -0500

    Fix a syntax error (extra paren)

--- elisp/config.d/mailcrypt-config.el
+++ elisp/config.d/mailcrypt-config.el
@@ -8,4 +8,4 @@
 ;; forcing these packages to load.
 (add-hook 'gnus-summary-mode-hook 'mc-install-read-mode) ; gnus
 (add-hook 'message-mode-hook 'mc-install-write-mode)     ; message-mail
-(add-hook 'news-reply-mode-hook 'mc-install-write-mode)) ; supercite
+(add-hook 'news-reply-mode-hook 'mc-install-write-mode)  ; supercite
