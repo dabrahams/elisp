eac36942d03fc14e232a295c174e0db46e89abf1
David Abrahams <dave@boostpro.com>
2009-01-07 11:33:19 -0500

    Made auto-recompilation more resilient to window height restrictions

--- elisp/autoload.d/lisp-mode-setup.el
+++ elisp/autoload.d/lisp-mode-setup.el
@@ -1,23 +1,44 @@
 ;; Snarfed from PierreJeanTurpeau at
 ;; http://www.emacswiki.org/emacs/AutoRecompile
 ;;
-;; He writes: this one tries to be less distracting by pre-splitting
-;; the window with a smaller *Compile-Log* buffer, auto-scroll to the
-;; last line to quickly show a potential error and then auto-close the
-;; *Compile-Log* window. It would be better to have a real error
-;; detection with a notification message rather than a dumb
-;; auto-scroll. Next time perhaps.
+;; He writes: 
+;;
+;;   this one tries to be less distracting by pre-splitting
+;;   the window with a smaller *Compile-Log* buffer, auto-scroll to the
+;;   last line to quickly show a potential error and then auto-close the
+;;   *Compile-Log* window. It would be better to have a real error
+;;   detection with a notification message rather than a dumb
+;;   auto-scroll. Next time perhaps.
+;;
+;; I've modified it a bit to make it more resilient to window height
+;; restrictions.
+(require 'cl)
 
 (defun pj/auto-recompile-file-always ()
+  (interactive)
   (when (equal mode-name "Emacs-Lisp")
     (let ((maximum-compile-log-height 8)
           (old-window-start (window-start))
-          (old-window-point (window-point)))
-      ;; pre-split for compile log buffer so that it does not modify the layout
-      (set-window-buffer (split-window-vertically (- (window-height) maximum-compile-log-height)) (get-buffer-create "*Compile-Log*"))
-      ;; byte compile the buffer
-      (byte-compile-file buffer-file-name)
-      (let ((buf (get-buffer "*Compile-Log*")))
+          (old-window-point (window-point))
+          (buf (get-buffer-create "*Compile-Log*"))
+          )
+
+      ;; temporarily override minimum window height; split if there's
+      ;; any way we can do it.
+      (letf ((window-min-height 1))
+        ;; Attempt to pre-split for compile log buffer so that it does
+        ;; not modify the layout.  If there is not even enough space for
+        ;; a window split of height 1, just itnore the error and don't
+        ;; split.
+        (ignore-errors
+          (set-window-buffer 
+           (split-window-vertically 
+            (max 1 (/ (window-height) 2) (- (window-height) maximum-compile-log-height)))
+           buf)
+         )
+
+        ;; byte compile the buffer
+        (byte-compile-file buffer-file-name)
         ;; scroll to the end to see if there's an error
         (set-window-point (get-buffer-window buf) (buffer-size buf))
         ;; auto close the compile log window and restore original display position
