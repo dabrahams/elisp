48d0eab79e75f88d37e231a13e32f5e6c796c77a
David Abrahams <dave@boostpro.com>
2009-01-09 10:07:15 -0500

    Split windows horizontally by default.  Yay!

--- elisp/config.d/more-init.el
+++ elisp/config.d/more-init.el
@@ -6,3 +6,12 @@
 
 ;; turn off the nasty system bell
 (setq ring-bell-function (lambda ()))
+
+(defadvice split-window (before by-default-split-horizontally activate)
+  "When the system is going to split the window and we have lots
+of horizontal space, I want to split it horizontally!  The
+default of splitting vertically (i.e. with a horizontal divider)
+leaves me looking at really long lines through really narrow
+spaces, which kinda blows."
+  (if (and (not (ad-get-arg 2)) (> (window-width (or (ad-get-arg 0) (selected-window)) 160)))
+      (ad-set-arg 2 t)))
