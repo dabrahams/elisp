cc0f08d31794f267206fb5d35d4f2fdc1fcbd0e5
David Abrahams <dave@boostpro.com>
2009-01-10 19:29:48 -0500

    Fix misplaced paren

--- elisp/config.d/more-init.el
+++ elisp/config.d/more-init.el
@@ -13,5 +13,5 @@ of horizontal space, I want to split it horizontally!  The
 default of splitting vertically (i.e. with a horizontal divider)
 leaves me looking at really long lines through really narrow
 spaces, which kinda blows."
-  (if (and (not (ad-get-arg 2)) (> (window-width (or (ad-get-arg 0) (selected-window)) 160)))
+  (if (and (not (ad-get-arg 2)) (> (window-width (or (ad-get-arg 0) (selected-window))) 160))
       (ad-set-arg 2 t)))
