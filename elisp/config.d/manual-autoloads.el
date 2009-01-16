f86c26a684873431146d08ff17997344d5af3ba2
David Abrahams <dave@boostpro.com>
2009-01-16 14:52:06 -0500

    Nice comment

--- elisp/config.d/manual-autoloads.el
+++ elisp/config.d/manual-autoloads.el
@@ -1,2 +1,5 @@
+;; This file contains autoloads that perhaps ought to have been
+;; embedded in packages I'm using, but weren't.
+
 (autoload 'magit-status "magit"
   "magit package supplies no doc." t)
