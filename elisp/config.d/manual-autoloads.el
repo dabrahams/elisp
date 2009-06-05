628bbd46bec33ace6a61779878cfdff5fe141799
David Abrahams <dave@zreba.local>
2009-06-05 15:35:41 -0400

    Add cmake support

--- elisp/config.d/manual-autoloads.el
+++ elisp/config.d/manual-autoloads.el
@@ -3,3 +3,13 @@
 
 (autoload 'magit-status "magit"
   "magit package supplies no doc." t)
+
+(autoload 'cmake-mode "cmake-mode"
+  "Major mode for editing CMake listfiles." t)
+
+(setq auto-mode-alist
+	  (append
+	   '(("CMakeLists\\.txt\\'" . cmake-mode))
+	   '(("\\.cmake\\'" . cmake-mode))
+	   auto-mode-alist))
+
