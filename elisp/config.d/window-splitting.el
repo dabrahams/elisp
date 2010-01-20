75fb47d2deedb7a98a658d057e6cdca01d4a4c6a
David Abrahams <dave@zreba.local>
2010-01-20 08:19:20 -0500

    When turning a vertical split into a horizontal one, ignore the number of lines specified.

--- elisp/config.d/window-splitting.el
+++ elisp/config.d/window-splitting.el
@@ -5,8 +5,10 @@ of horizontal space, I want to split it horizontally!  The
 default of splitting vertically (i.e. with a horizontal divider)
 leaves me looking at really long lines through really narrow
 spaces, which kinda blows."
-    (if (and (not (ad-get-arg 2)) (> (window-width (or (ad-get-arg 0) (selected-window))) 160))
-        (ad-set-arg 2 t)))
+    (when (and (not (ad-get-arg 2)) (> (window-width (or (ad-get-arg 0) (selected-window))) 160))
+        (ad-set-arg 2 t)
+        (ad-set-arg 1 nil)
+        ))
 
   (defadvice split-window-vertically (around explicit-split-vertically activate protect)
     "See split-window advice \"by-default-split-horizontally\".
